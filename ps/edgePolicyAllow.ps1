84532d0c4d03dc36e10555718091f662916981d0
ulyweb <uly@ulynetwork.com>
2025-04-08 13:42:27 -0700

    Update edgePolicyAllow.ps1

--- ps/edgePolicyAllow.ps1
+++ ps/edgePolicyAllow.ps1
@@ -1,5 +1,16 @@
 
 
+# Ensure the registry path exists first
+if (-not (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge")) {
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Force | Out-Null
+}
+
+# Set Edge policies for automatic sign-in and sync
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "BrowserSignin" -Value 1 -PropertyType DWord -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "ForceSync" -Value 1 -PropertyType DWord -Force
+
+
+
 # Getting info only
 # Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge"
 
@@ -7,9 +18,7 @@ New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "SyncDisab
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "PasswordManagerEnabled" -Value 1 -PropertyType DWord -Force
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "PasswordMonitorAllowed" -Value 1 -PropertyType DWord -Force
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "SyncTypesListDisabled" -PropertyType String -Value "" -Force
-
-
-
+																	 
 
 # Enable "Save and fill basic info" (required to unlock ML autofill toggle)
 New-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Edge" -Name "AutofillAddressEnabled" -Value 1 -PropertyType DWord -Force
